269c083226250d3c2b1bcec050fd2c71dec579ff
Ricky0625 <rickywong0625@gmail.com>
2023-09-19 18:47:54 +0800

    deleted pushed folder

--- python-0-starting/ex09/ft_package/__init__.py
+++
@@ -1,3 +0,0 @@
-from .count_in_list import count_in_list
-
-_ = count_in_list
--- python-0-starting/ex09/ft_package/count_in_list.py
+++
@@ -1,9 +0,0 @@
-def count_in_list(lst: list, key: str) -> int:
-
-    """
-    count_in_list(lst: list, key, str) -> int
-
-    Return the number of occurrence of a string in a list.
-    """
-
-    return lst.count(key)
